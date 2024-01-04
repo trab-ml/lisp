920262b6c2298f4a7f576cda310900698d709e7d
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2024-01-04 11:41:57 +0100

    :sparkles: version 2024

--- checklinks.sh
+++ checklinks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -d "TDD2023TESTS" ]; then
+if [ ! -d "TDD2024TESTS" ]; then
    echo "Adding main public test repository for branch $2"
-   git submodule add -b $2 --force https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.univ-artois.fr/dlbenseignement/m1-2022-2023/TDD2023TESTS.git 
+   git submodule add -b $2 --force https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.univ-artois.fr/dlbenseignement/m1-2023-2024/TDD2024TESTS.git 
 fi
 
 if [ ! -d "TDD2021HIDDENTESTS" ]; then
@@ -10,9 +10,9 @@ if [ ! -d "TDD2021HIDDENTESTS" ]; then
    git submodule add -b $2 --force https://oauth2:cnubdNfHsLYzw-jZB23_@gitlab.univ-artois.fr/dlbenseignement/m1-2020-2021/TDD2021HIDDENTESTS.git 
 fi
 
-if [ ! -d "TDD2023OWNTESTS" ]; then
+if [ ! -d "TDD2024OWNTESTS" ]; then
    echo "Adding own test repository for $1 for branch $2"
-   git submodule add -b $2 --force https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.univ-artois.fr/$1/TDD2023OWNTESTS.git 
+   git submodule add -b $2 --force https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.univ-artois.fr/$1/TDD2024OWNTESTS.git 
 
 fi
 
