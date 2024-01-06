e99337e62b871eef3a620ab85936132ce63eb2c3
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2024-01-06 17:55:41 +0100

    :lock: changement accès aux tests cachés

--- checklinks.sh
+++ checklinks.sh
@@ -7,7 +7,7 @@ fi
 
 if [ ! -d "TDD2021HIDDENTESTS" ]; then
    echo "Adding main hidden test repository for branch $2" 
-   git submodule add -b $2 --force https://oauth2:cnubdNfHsLYzw-jZB23_@gitlab.univ-artois.fr/dlbenseignement/m1-2020-2021/TDD2021HIDDENTESTS.git 
+   git submodule add -b $2 --force https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.univ-artois.fr/dlbenseignement/m1-2020-2021/TDD2021HIDDENTESTS.git 
 fi
 
 if [ ! -d "TDD2024OWNTESTS" ]; then
