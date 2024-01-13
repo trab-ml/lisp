d84d1ea1925980d556443b51e9bfb54d532732ff
Traore Bernard <bernard_traore@ens.univ-artois.fr>
2024-01-13 16:11:08 +0100

    Implement size, prepend, isEmpty, car and cdr functions

--- src/main/java/vvl/util/ConsListImpl.java
+++ src/main/java/vvl/util/ConsListImpl.java
@@ -8,13 +8,25 @@ import org.checkerframework.common.returnsreceiver.qual.This;
 public class ConsListImpl<E> implements ConsList<E> {
 	
 	private final Cons<E, ConsList<E>> list;
-	
+	private int size = 0;
+
 	public ConsListImpl() {
-		this(null);
+		this.list = null;
 	}
 	
 	public ConsListImpl(Cons<E, ConsList<E>> list) {
 		this.list = list;
+		this.size = computeSize();
+	}
+	
+	private int computeSize() {
+	    int cpt = 0;
+	    ConsList<E> current = this;
+	    while (!current.isEmpty()) {
+	    	cpt++;
+	        current = current.cdr();
+	    }
+	    return cpt;
 	}
 
 	@Override
@@ -25,37 +37,41 @@ public class ConsListImpl<E> implements ConsList<E> {
 
 	@Override
 	public ConsList<E> prepend(E e) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ConsListImpl<>(new Cons<>(e, this));
 	}
 
 	@Override
-	public ConsList<E> append(E e) {
-		// TODO Auto-generated method stub
+    public ConsList<E> append(E e) {
 		return null;
-	}
+    }
 	
 	@Override
 	public boolean isEmpty() {
-		return false;
+//		System.out.println("this.list --> " + this.list);
+//		System.out.println("ConsList.nil() --> " + ConsList.nil());
+		return this.list == null;
 	}
 
 	@Override
-	public E car() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public E car() {
+        if (list == null) {
+            throw new UnsupportedOperationException("car() called on an empty list");
+        }
+        return list.left();
+    }
 
 	@Override
 	public ConsList<E> cdr() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        if (list == null) {
+            throw new UnsupportedOperationException("cdr() called on an empty list");
+        }
+        return list.right();
+    }
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+//		System.out.println("ConsList current size: " + size);
+		return size;
 	}
 
 	@Override
