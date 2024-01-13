8754086a1952ea1d80d8925e8f532fb2f17d8193
Traore Bernard <bernard_traore@ens.univ-artois.fr>
2024-01-13 13:16:26 +0100

    Initialize ConsListImpl

---
+++ src/main/java/vvl/util/ConsListImpl.java
@@ -0,0 +1,67 @@
+package vvl.util;
+
+import java.util.Iterator;
+import java.util.function.Function;
+
+import org.checkerframework.common.returnsreceiver.qual.This;
+
+public class ConsListImpl<E> implements ConsList<E> {
+	
+	private final Cons<E, ConsList<E>> list;
+	
+	public ConsListImpl() {
+		this(null);
+	}
+	
+	public ConsListImpl(Cons<E, ConsList<E>> list) {
+		this.list = list;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConsList<E> prepend(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConsList<E> append(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public E car() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConsList<E> cdr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <T> ConsList<T> map(Function<E, T> f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
